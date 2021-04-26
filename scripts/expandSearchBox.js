d363df8e8e15fb706739626dc37b0983196bd7aa
altnum <altnum16@gmail.com>
2021-04-27 00:56:49 +0300

    Transition of searchbox and visibility of topVIP ads synchronized

--- scripts/expandSearchBox.js
+++ scripts/expandSearchBox.js
@@ -1,11 +1,17 @@
 var box = document.getElementsByClassName("searchbox");
 var button = document.getElementsByClassName("detailedsearch");
+var topVip = document.getElementsByClassName("topVip");
+
 box[0].style.maxHeight = 200 + "px";
 button[0].addEventListener("click", function() {
     
     if (box[0].style.maxHeight != "200px") {
         box[0].style.maxHeight = 200 + "px";
+        setTimeout(function() {
+            topVip[0].style.visibility = "visible";
+        }, 500);
     } else {
+        topVip[0].style.visibility = "hidden";
         box[0].style.maxHeight = 600 + "px";
     }
 });
