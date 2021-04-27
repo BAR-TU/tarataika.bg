4992034c88fce28a4721aec943411a0400cd437d
altnum <altnum16@gmail.com>
2021-04-27 14:07:37 +0300

    Lis are now buttons

---
+++ scripts/navButtons.js
@@ -0,0 +1,11 @@
+function clickTrivia() {
+    location.href = "trivia.html";
+}
+
+function clickHome() {
+    location.href = "index.html";
+}
+
+function clickTest() {
+    location.href = "vehicletemplate.html";
+}
