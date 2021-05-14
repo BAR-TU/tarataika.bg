b3cf7bdd7adf0a2236899b7f6bccb2e966a120ae
altnum <altnum16@gmail.com>
2021-05-14 12:21:12 +0300

    Tide

--- scripts/map.js
+++ scripts/map.js
@@ -1,7 +1,7 @@
 // Initialize and add the map
 function initMap() {
     // The location of Uluru
-    const uluru = { lat: -25.344, lng: 131.036 };
+    const uluru = { lat: -20.344, lng: 112.036 };
     // The map, centered at Uluru
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 4,
